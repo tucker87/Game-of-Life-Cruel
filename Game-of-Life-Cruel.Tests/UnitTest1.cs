96396f4661006d654bf9970cfcc1879d1e163d05
Jason Tucker <jtgeek@gmail.com>
2018-05-05 15:17:26 -0400

    The rest

---
+++ Game-of-Life-Cruel.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace Game_of_Life_Cruel.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CanGame()
+        {
+            //Arrange
+            var game = new GameOfLife();
+            //Act
+            var result = game.handleSubmit();
+
+
+
+            result.MoveNext();
+
+            //Assert
+        }
+    }
+}
